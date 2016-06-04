7db0a987f0c5b3b922fc93882e7580a819c8e577
eraether <eugene.raether@yahoo.com>
2016-06-04 12:06:44 -0400

    Remove slack token

--- src/main/java/com/raether/watchwordbot/Runner.java
+++ src/main/java/com/raether/watchwordbot/Runner.java
@@ -3,8 +3,11 @@ package com.raether.watchwordbot;
 public class Runner {
 
 	public static void main(String[] args) {
-		WatchWordBot bot = new WatchWordBot(
-				"xoxb-47186420693-Ham8g4SWPiR8CSPYgMEhlujK");
+		if(args.length == 0){
+			System.err.println("Please pass in your slack token code as an argument.");
+			System.exit(1);
+		}
+		WatchWordBot bot = new WatchWordBot(args[0]);
 		try {
 			bot.loadWordList();
 			bot.connect();
